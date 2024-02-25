49671b76f45f1a7cb233683278a6c81fbecb3663
Olivier Delmas <olivier.delmas@edu.devinci.fr>
2024-02-25 17:14:44 +0100

    :fire: finished first part of final task

--- back/controllers/ChampionsController.js
+++ back/controllers/ChampionsController.js
@@ -48,11 +48,40 @@ const createChampions = (req, res) => {
 }
 
 const updateChampions = (req, res) => {
-    //
+    let idChampion = Number(req.params.id)
+    let champion = req.body
+
+    prisma.champions.update({
+        where: {
+            id: idChampion
+        }, 
+        data: {
+            name: champion.name,
+            type: champion.type
+        }
+    })
+    .then((champion) => {
+        res.json(champion)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
 }
 
 const deleteChampions = (req, res) => {
-    //
+    let idChampion = Number(req.params.id)
+
+    prisma.champions.delete({
+        where: {
+            id: idChampion
+        }
+    })
+    .then((champion) => {
+        res.json(champion)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
 }
 
 export { getChampions, getAllChampions, createChampions, updateChampions, deleteChampions }
