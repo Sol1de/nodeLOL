65568b7e99e32ccb0f90ea119744e8019c47a4e2
Olivier Delmas <olivier.delmas@edu.devinci.fr>
2024-02-25 16:13:42 +0100

    :fire: added createChampion function + vue on index.html (need to be fixed)

--- back/controllers/ChampionsController.js
+++ back/controllers/ChampionsController.js
@@ -1,13 +1,38 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma =  new PrismaClient()
+
 const getChampions = (req, res) => {
     //
 }
 
 const getAllChampions = (req, res) => {
-    res.send(['example 1', 'example 2', 'example 3'])
+    prisma.champions.findMany()
+    .then((champion) => {
+        res.json(champion)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
+
 }
 
 const createChampions = (req, res) => {
-    //
+    let champion = req.body
+    console.log(champion)
+
+    prisma.champions.create({
+        data: {
+            name: champion.name,
+            type: champion.type
+        }
+    })
+    .then((champion) => {
+        res.json(champion)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
 }
 
 const updateChampions = (req, res) => {
--- back/index.js
+++ back/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 import router from './router.js'
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(router)
 
 app.listen(3000, () => {
